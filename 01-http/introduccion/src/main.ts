7ece6f812ac885c2faf76da1fe4d8fc6917b55ab
Andrés Pantoja <andres.pantoja@epn.edu.ec>
2020-07-03 16:01:21 -0500

    modulo inicio

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /*
+  * Configuraciones
+  * antes del app.listen()
+  * */
+  await app.listen(3001);
 }
 bootstrap();
